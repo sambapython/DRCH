ad88c6bb55b38f8ccc31f4d9257dbc52d0a191c9
sambaodoo@gmail.com <sambaodoo@gmail.com>
2019-05-29 14:23:02 +0530

    converted to mysql db

--- drch/settings.py
+++ drch/settings.py
@@ -73,11 +73,20 @@ WSGI_APPLICATION = 'drch.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'db.cnf',
+        },
     }
 }
 
